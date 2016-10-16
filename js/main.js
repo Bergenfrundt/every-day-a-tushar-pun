92fe85ba4f4bd1bcaac425b3b582a6bce4f9148f
Damanjit <thepizzapop22@outlook.com>
2016-10-16 14:14:21 -0400

    typo.jpg

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ addEventListener('load', function () {
 		// Put your puns as strings in this array
 		"What do you call it when you cut yourself on tushar? Tusharp.",
 		"What do you call it when tushar has more than one vehicle, but less than three? Tucar.",
-		"What do you call it when tushar gets a good score in gold multiple times? Tupar.",
+		"What do you call it when tushar gets a good score in golf multiple times? Tupar.",
 		"What do you call it when tushar opens multiple pubs? Tubar.",
 		"What do you call it when tushar has multiple containers? Tujar.",
 		"What do you call it when tushar makes an incredibly offensive joke? Tufar."
